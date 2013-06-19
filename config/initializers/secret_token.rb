e67b2948f053e40ab7e079246a336287bb6ebd87
Zachary Bryan <zachx1@gmail.com>
2013-06-18 21:16:11 -0700

    initial commit prior to bershelf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'a4818dfa0a6dd8863a7a3f13b44379ce9483f1b7f2439908b99db3b98abce955ff0c3b79b84af402d6bb61921917e4f2c65eacde772d5642fa7aa1ed7a0fa50d'
